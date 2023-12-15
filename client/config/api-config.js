5216c212a2f210dd9397b9a98e4bc3533fe82568
Peter Carter <139152930+peterhippocarter@users.noreply.github.com>
2023-12-15 16:09:27 +0000

    Update api-config.js

--- client/config/api-config.js
+++ client/config/api-config.js
@@ -1,3 +1,3 @@
-// If we want to simulate IRL conditions, this won't be included in any documentation so the students will need to ask around (e.g.g ask the task moderator) to get this.
+// If we want to simulate IRL conditions, this won't be included in any documentation so the students will need to ask around (e.g. ask the task moderator) to get this.
 // Might want to ask Jonny about this one but we could put something like 'ask your invigilator if you're unsure about this question'.
 export const apiBasePath = process.env.BASEPATH;
